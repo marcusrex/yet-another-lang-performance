03ba5ae76bcc72307cea5fab1d8057eb354ef7bb
Arthur <virid.raven@gmail.com>
2017-09-16 16:24:08 +0500

    Oprimised prime benchmarks were added

---
+++ yet-another-lang-performance/python/benchmarks/alternative_benchmarks/rwh_primesfrom2to_benchmark.py
@@ -0,0 +1,26 @@
+import numpy
+
+from benchmarks.benchmark import Benchmark
+
+
+class RwhPrimesFrom2toBenchmark(Benchmark):
+    primes_number = 10
+
+    @staticmethod
+    def __primesfrom2to(n):
+        """ https://stackoverflow.com/questions/2068372/fastest-way-to-list-all-primes-below-n-in-python/3035188#3035188"""
+        """ Input n>=6, Returns a array of primes, 2 <= p < n """
+        sieve = numpy.ones(int(n / 3 + (n % 6 == 2)), dtype=numpy.bool)
+        for i in range(1, int(int(n ** 0.5) / 3 + 1)):
+            if sieve[i]:
+                k = 3 * i + 1 | 1
+                sieve[int(k * k / 3)::int(2 * k)] = False
+                sieve[int(k * (k - 2 * (i & 1) + 4) / 3)::int(2 * k)] = False
+        return numpy.r_[2, 3, ((3 * numpy.array(numpy.nonzero(sieve))[0][1:] + 1) | 1)]
+
+    def __init__(self, primes_number: int):
+        self.primes_number = primes_number
+        super().__init__("RWH primesfrom2")
+
+    def _execute_impl(self):
+        self.__primesfrom2to(self.primes_number)
--- yet-another-lang-performance/python/main.py
+++ yet-another-lang-performance/python/main.py
@@ -1,16 +1,24 @@
 # -*- coding: UTF-8 -*-
+import itertools
+
 from tabulate import tabulate
 
+from benchmarks.alternative_benchmarks import rwh_primes_benchmark, rwh_primesfrom2to_benchmark
 from benchmarks.bad_benchmarks import primes_benchmark
 from benchmarks.benchmark_collection import BenchmarkCollection
 
 MAX_PRIME = 10000000
 
-bad_benchmarks = BenchmarkCollection("Bad benchmarks",
-                                     [
-                                         primes_benchmark.PrimesBenchmark(MAX_PRIME)
-                                     ]
+bad_benchmarks = BenchmarkCollection("Bad benchmarks", [
+    primes_benchmark.PrimesBenchmark(MAX_PRIME)
+]
                                      )
+alternative_benchmarks = BenchmarkCollection("Alternative benchmarks", [
+    rwh_primes_benchmark.RwhPrimesBenchmark(MAX_PRIME),
+    rwh_primesfrom2to_benchmark.RwhPrimesFrom2toBenchmark(MAX_PRIME),
+
+]
+                                             )
 
 
 def print_results(results: list):
@@ -20,7 +28,7 @@ def print_results(results: list):
 if __name__ == "__main__":
     benchmark_results = []
 
-    for bench in bad_benchmarks:
+    for bench in itertools.chain(bad_benchmarks, alternative_benchmarks):
         benchmark_results.append(bench.execute())
 
     print_results(benchmark_results)
